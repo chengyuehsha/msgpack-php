f168f209c451265e557f444f48facf93913a111a
chengyuehsha <chengyueh.sha@gmail.com>
2022-08-10 21:39:01 +0800

    Add my Packer class

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
+use Chengyueh\MsgPack\Packer;
 use PHPUnit\Framework\TestCase;
 
 class FirstTest extends TestCase
 {
     public function testExample(): void
     {
-        $this->assertTrue(true);
+        $this->assertEquals('world', Packer::hello());
     }
 }
