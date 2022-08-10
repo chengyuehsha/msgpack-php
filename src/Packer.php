02b7aefe6ac95fc6fcfb5d2560fbf2b4525633c2
chengyuehsha <chengyueh.sha@gmail.com>
2022-08-10 21:54:47 +0800

    Add bool format and test

--- src/Packer.php
+++ src/Packer.php
@@ -6,6 +6,11 @@ namespace Chengyueh\MsgPack;
 
 class Packer
 {
+    public static function bool(bool $val): int
+    {
+        return ($val) ? 0xC3 : 0xC2;
+    }
+
     public static function nil($val): int
     {
         return 0xC0;
--- tests/FormatTest.php
+++ tests/FormatTest.php
@@ -8,6 +8,17 @@ use Symfony\Component\Yaml\Yaml;
 
 class FormatTest extends TestCase
 {
+    public function testBool(): void
+    {
+        $cases = Yaml::parseFile(__DIR__ . '/../msgpack-test-suite/src/11.bool.yaml');
+        foreach ($cases as $case) {
+            $input = $case['bool'];
+            $expect = $case['msgpack'][0];
+
+            $this->assertEquals($expect, dechex(Packer::bool($input)));
+        }
+    }
+
     public function testNull(): void
     {
         $case = Yaml::parseFile(__DIR__ . '/../msgpack-test-suite/src/10.nil.yaml');
